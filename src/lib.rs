b7a4a648390c4ca79f978dfcc48785e677073b6a
impact <impactdni@gmail.com>
2024-11-19 08:57:05 -0500

    using the plugin base from yellowstone

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,85 @@
-use agave_geyser_plugin_interface::geyser_plugin_interface::GeyserPlugin;
+use 
+  agave_geyser_plugin_interface::geyser_plugin_interface::{
+      GeyserPlugin, ReplicaAccountInfoVersions, ReplicaBlockInfoVersions,
+      ReplicaEntryInfoVersions, ReplicaTransactionInfoVersions, Result as PluginResult,
+      SlotStatus,
+  };
 
-mod plugin;
-pub use plugin::SimplePlugin;
+
+#[derive(Debug, Default)]
+pub struct Plugin {
+}
+
+impl GeyserPlugin for Plugin {
+  fn name(&self) -> &'static str {
+    "dummy-geyser"
+  }
+
+  fn on_load(&mut self, _config_file: &str, _is_reload: bool) -> PluginResult<()> {
+      Ok(())
+  }
+
+  fn on_unload(&mut self) {
+  }
+
+  fn update_account(
+      &self,
+      _account: ReplicaAccountInfoVersions,
+      _slot: u64,
+      _is_startup: bool,
+  ) -> PluginResult<()> {
+      Ok(())
+  }
+
+  fn notify_end_of_startup(&self) -> PluginResult<()> {
+      Ok(())
+  }
+
+  fn update_slot_status(
+      &self,
+      _slot: u64,
+      _parent: Option<u64>,
+      _status: SlotStatus,
+  ) -> PluginResult<()> {
+      Ok(())
+  }
+
+  fn notify_transaction(
+      &self,
+      _transaction: ReplicaTransactionInfoVersions<'_>,
+      _slot: u64,
+  ) -> PluginResult<()> {
+      Ok(())
+  }
+
+  fn notify_entry(&self, _entry: ReplicaEntryInfoVersions) -> PluginResult<()> {
+      Ok(())
+  }
+
+  fn notify_block_metadata(&self, _blockinfo: ReplicaBlockInfoVersions<'_>) -> PluginResult<()> {
+      Ok(())
+  }
+
+  fn account_data_notifications_enabled(&self) -> bool {
+    false
+  }
+
+  fn transaction_notifications_enabled(&self) -> bool {
+    false
+  }
+
+  fn entry_notifications_enabled(&self) -> bool {
+    false
+  }
+}
 
 #[no_mangle]
 #[allow(improper_ctypes_definitions)]
 /// # Safety
 ///
-/// The Solana validator and this plugin must be compiled with the same Rust compiler version and Solana core version.
-/// Loading this plugin with mismatching versions is undefined behavior and will likely cause memory corruption.
+/// This function returns the Plugin pointer as trait GeyserPlugin.
 pub unsafe extern "C" fn _create_plugin() -> *mut dyn GeyserPlugin {
-    let plugin: Box<dyn GeyserPlugin> = Box::new(SimplePlugin::default());
-    Box::into_raw(plugin)
+  let plugin = Plugin::default();
+  let plugin: Box<dyn GeyserPlugin> = Box::new(plugin);
+  Box::into_raw(plugin)
 }
